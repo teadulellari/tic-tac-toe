30a0d778331ba8bade0cf3976af2b0afde153ab8
teadulellari <teadulellari@gmail.com>
2023-04-28 00:04:23 +0300

    creating multiplayer functionality and win-draw scenario

--- tic-tac-toe/src/components/Game.tsx
+++ tic-tac-toe/src/components/Game.tsx
@@ -1,10 +1,72 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Square from "./Square";
 
-const initialGameState = ["X", "", "", "", "", "", "", "", ""];
-
+const initialGameState = ["", "", "", "", "", "", "", "", ""];
+const winningCombos = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6],
+];
 const Game = () => {
   const [gameState, setGameState] = useState(initialGameState);
+  const [currentPlayer, setCurrentPlayer] = useState("X");
+
+  useEffect(() => {
+   checkForWinner();
+  }, [gameState])
+  
+  const checkForWinner = () => {
+   let roundWon = false;
+
+   for(let i=0; i<winningCombos.length; i++) {
+    const winCombo = winningCombos[i];
+    let a = gameState[winCombo[0]];
+    let b = gameState[winCombo[1]];
+    let c = gameState[winCombo[2]];
+
+    if ([a, b, c].includes("")) {
+      continue;
+    }
+
+    if(a === b && b === c ){
+      roundWon = true;
+      break;
+
+    }
+   }
+   if (roundWon) {
+    window.alert(`Congrats player ${currentPlayer}! You are the winner!`);
+    return
+   }
+   if(!gameState.includes("")){
+    window.alert("The game ended in a draw");
+    return
+   }
+   changePlayer();
+  };
+
+  const changePlayer = () => {
+    setCurrentPlayer(currentPlayer === "X" ? "O" : "X" )
+  }
+
+  const handleclick = (event: any) => {
+    
+    const cellIndex = Number(event.target.getAttribute("data-cell-index"));
+    const currentValue = gameState[cellIndex];
+    if(currentValue) {
+      return;
+    }
+
+    const newValues = [...gameState];
+    newValues[cellIndex] = currentPlayer;
+    setGameState(newValues);
+    console.log("file: Game.tsx ~ line 14 ~ handleClick ~ currentValue", currentValue);
+  };
   return (
     <div className="h-full p-8 text-slate-800 bg-gradient-to-r from-cyan-500 to-blue-500">
       <h1 className="text-center text-5xl mb-4 font-display text-white">
@@ -13,7 +75,7 @@ const Game = () => {
       <div>
         <div className="grid grid-cols-3 gap-3 mx-auto w-96">
           {gameState.map((player, index) => (
-            <Square key={index} {...{player}} />
+            <Square key={index} onClick={handleclick} {...{index, player}} />
           ))}
         </div>
         <div>Scores Go Here</div>
